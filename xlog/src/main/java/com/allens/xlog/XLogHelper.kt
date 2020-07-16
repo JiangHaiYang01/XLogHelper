bc1edeb52e1eccc8c0a0216e7b84092d54b25b2b
allens <18856907654@#163.com>
2020-07-16 20:26:56 +0800

    add log i v e ...

--- xlog/src/main/java/com/allens/xlog/XLogHelper.kt
+++ xlog/src/main/java/com/allens/xlog/XLogHelper.kt
@@ -16,6 +16,45 @@ object XLogHelper {
         return Builder(context)
     }
 
+    fun v(msg: String) {
+        v(msg, null)
+    }
+
+
+    fun v(format: String, vararg obj: Any?) {
+        Log.v(Builder.tag, format, *obj)
+    }
+
+
+    fun f(msg: String) {
+        f(msg, null)
+    }
+
+
+    fun f(format: String, vararg obj: Any?) {
+        Log.f(Builder.tag, format, *obj)
+    }
+
+
+    fun e(msg: String) {
+        e(msg, null)
+    }
+
+
+    fun e(format: String, vararg obj: Any?) {
+        Log.e(Builder.tag, format, *obj)
+    }
+
+
+    fun w(msg: String) {
+        w(msg, null)
+    }
+
+
+    fun w(format: String, vararg obj: Any?) {
+        Log.w(Builder.tag, format, *obj)
+    }
+
 
     fun i(msg: String) {
         i(msg, null)
@@ -24,6 +63,16 @@ object XLogHelper {
 
     fun i(format: String, vararg obj: Any?) {
         Log.i(Builder.tag, format, *obj)
+    }
+
+
+    fun d(msg: String) {
+        d(msg, null)
+    }
+
+
+    fun d(format: String, vararg obj: Any?) {
+        Log.d(Builder.tag, format, *obj)
     }
 
 
